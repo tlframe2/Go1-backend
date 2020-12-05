4d467148806dbc57081b23ab7b814c3e0d3fe849
Tyrell <tlframe2@gmail.com>
2020-12-05 13:46:49 -0500

    initial commit

---
+++ events.js
@@ -0,0 +1,138 @@
+module.exports = ([
+  {
+    Title: "Infection Prevention and Control (Australia)",
+    Time: "2021-03-22T02:30:00.000Z",
+    Image: "http://example.com/image.png",
+    Location: {
+      City: "Brisbane",
+      State: "Queensland",
+      Country: "Australia",
+    }
+  },
+  {
+    Title: "Wellbeing First - Mindfulness",
+    Time: "2021-03-24T02:30:00.000Z",
+    Image: "http://example.com/image.png",
+    Location: {
+      City: "Cairns",
+      State: "Queensland",
+      Country: "Australia",
+    },
+    AvailableSeats: [
+      {
+        id: "W25"
+      },
+      {
+        id: "B29"
+      }
+    ]
+  },
+  {
+    Title: "10 Minutes Managing Stress",
+    Time: "2021-03-24T02:30:00.000Z",
+    Image: "http://example.com/image.png",
+    Location: {
+      City: "Gold Coast",
+      State: "Queensland",
+      Country: "Australia",
+    },
+    AvailableSeats: [
+      {
+        id: "W25"
+      },
+      {
+        id: "B29"
+      }
+    ]
+  },
+  {
+    Title: "10 Minute Pandemic Awareness",
+    Time: "2021-03-24T02:30:00.000Z",
+    Image: "http://example.com/image.png",
+    Location: {
+      City: "Gold Coast",
+      State: "Queensland",
+      Country: "Australia",
+    },
+    AvailableSeats: [
+      {
+        id: "W25"
+      },
+      {
+        id: "B29"
+      }
+    ]
+  },
+  {
+    Title: "Morning Yoga Kickstart",
+    Time: "2021-03-24T02:30:00.000Z",
+    Image: "http://example.com/image.png",
+    Location: {
+      City: "Gold Coast",
+      State: "Queensland",
+      Country: "Australia",
+    },
+    AvailableSeats: [
+      {
+        id: "W25"
+      },
+      {
+        id: "B29"
+      }
+    ]
+  },
+  {
+    Title: "Hand Hygiene in the Workplace",
+    Time: "2021-03-24T02:30:00.000Z",
+    Image: "http://example.com/image.png",
+    Location: {
+      City: "Gold Coast",
+      State: "Queensland",
+      Country: "Australia",
+    },
+    AvailableSeats: [
+      {
+        id: "W25"
+      },
+      {
+        id: "B29"
+      }
+    ]
+  },
+  {
+    Title: "Equal Employment Opportunity",
+    Time: "2021-03-24T02:30:00.000Z",
+    Image: "http://example.com/image.png",
+    Location: {
+      City: "Gold Coast",
+      State: "Queensland",
+      Country: "Australia",
+    },
+    AvailableSeats: [
+      {
+        id: "W25"
+      },
+      {
+        id: "B29"
+      }
+    ]
+  },
+  {
+    Title: "First Aid - Basics",
+    Time: "2021-03-24T02:30:00.000Z",
+    Image: "http://example.com/image.png",
+    Location: {
+      City: "Gold Coast",
+      State: "Queensland",
+      Country: "Australia",
+    },
+    AvailableSeats: [
+      {
+        id: "W25"
+      },
+      {
+        id: "B29"
+      }
+    ]
+  },
+])
