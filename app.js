f3c2473191245a7b76f754305ab7b11ef21dd290
Tyrell <tlframe2@gmail.com>
2020-12-05 16:26:58 -0500

    added and tested api route that gets event by id

--- app.js
+++ app.js
@@ -10,4 +10,16 @@ app.get('/api/events', (req, res) => {
   res.status(200).send(events);
 });
 
+app.get('/api/events/:eventId', (req, res) => {
+  const eventId = parseInt(req.params.eventId);
+
+  if (eventId != null && eventId < events.length) {
+    const event = events[eventId];
+    res.send(event);
+  } else {
+    res.status(404).send('Could not find event with given id.');
+  }
+});
+
+
 module.exports = app;
--- tests/api.test.js
+++ tests/api.test.js
@@ -153,3 +153,61 @@ describe("Test getting all events route", () => {
     ]);
   });  
 });
+
+describe('Test getting single event by id', () => {
+  test("Using id 0 should return status code 200", async () => {
+    return await request(app).get("/api/events/0").expect(200);
+  });
+
+  test("Event with id 0 should return first object in events.js", async () => {
+    const { body } = await request(app).get("/api/events/0");
+    return expect(body).toEqual({
+      Title: "Infection Prevention and Control (Australia)",
+      Time: "2021-03-22T02:30:00.000Z",
+      Image: "http://example.com/image.png",
+      Location: {
+        City: "Brisbane",
+        State: "Queensland",
+        Country: "Australia",
+      }
+    });
+  });
+
+  test("Event with id 7 should return last object in events.js", async () => {
+    const { body } = await request(app).get("/api/events/7");
+    return expect(body).toEqual({
+      Title: "First Aid - Basics",
+      Time: "2021-03-24T02:30:00.000Z",
+      Image: "http://example.com/image.png",
+      Location: {
+        City: "Gold Coast",
+        State: "Queensland",
+        Country: "Australia",
+      },
+      AvailableSeats: [
+        {
+          id: "W25"
+        },
+        {
+          id: "B29"
+        }
+      ]
+    });
+  });
+
+  test("Using id 8 should return 404 status code", async () => {
+    return await request(app).get("/api/events/8").expect(404);
+  });
+
+  test("Using id 8 should return error message", async () => {
+    return await request(app).get("/api/events/8").expect('Could not find event with given id.');
+  });
+
+  test("Using id x should return 404 status code", async () => {
+    return await request(app).get("/api/events/x").expect(404);
+  });
+
+  test("Using id x should return error message", async () => {
+    return await request(app).get("/api/events/x").expect('Could not find event with given id.');
+  });
+});
